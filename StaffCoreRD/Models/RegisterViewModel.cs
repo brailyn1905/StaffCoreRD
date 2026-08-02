bcd623c632f6d5d9a5bd9fb3a54bfebead662a3b
brailyn ramirez jaquez <ramirezbrailyn55@gmail.com>
2026-08-02 13:33:37 -0400

    ViewModels RegisterViewModel y LoginViewModel

---
+++ StaffCoreRD/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StaffCoreRD.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
